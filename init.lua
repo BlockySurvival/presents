0c0c5b5439548ddfc6f5e84a1d7f57d1bb60016d
krypticbit <bbrus@comcast.net>
2020-12-09 11:59:25 -0500

    Increased stack size, changed drop rate

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 local pnum = 0
 
-local function register_present(c1, c2)
+local function register_present(c1, c2, lvl)
     minetest.register_node("presents:present_" .. tostring(pnum), {
-        description = "Christmas Present",
+        description = "Christmas Present " .. lvl,
         tiles = {
             "presents_blank.png^[multiply:" .. c1 .. "^(presents_bow.png^[multiply:" .. c2 .. ")",
             "presents_blank.png^[multiply:" .. c1 .. "^(presents_cross.png^[multiply:" .. c2 .. ")",
@@ -13,18 +13,26 @@ local function register_present(c1, c2)
         on_punch = function(pos, node, puncher, pointed_thing)
             if not puncher or not puncher:is_player() then return end
             minetest.chat_send_player(puncher:get_player_name(), "Bring this present to the Christmas shop to redeem it!")
-        end
+        end,
+        stack_max = 30000
     })
     pnum = pnum + 1
 end
 
-register_present("#bb2528", "#146b3a")
-register_present("#146b3a", "#bb2528")
-register_present("#bb2528", "#f0f0f0")
+register_present("#bb2528", "#146b3a", "(Rare)")
+register_present("#146b3a", "#bb2528", "(Unusual)")
+register_present("#bb2528", "#f0f0f0", "(Common)")
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
     if not digger or not digger:is_player() then return end
-    if math.random(1, 100) == 1 then
-        minetest.item_drop(ItemStack("presents:present_" .. tostring(math.random(0, pnum - 1))), digger, pos)
+    local n = math.random(1, 400)
+    if n <= 10 then
+        if n == 1 then
+            minetest.item_drop(ItemStack("presents:present_0"), digger, pos)
+        elseif n <= 3 then
+            minetest.item_drop(ItemStack("presents:present_1"), digger, pos)
+        else
+            minetest.item_drop(ItemStack("presents:present_2"), digger, pos)
+        end
     end
 end)
